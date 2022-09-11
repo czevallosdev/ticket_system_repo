33bef71d517067fa0235d128caaa047d95758c28
cristhianzevallos <cristhian.zevallos@sophos.com>
2022-09-11 15:01:04 +0100

    adding update and delete options

--- website/views.py
+++ website/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 
 from . import db
-from .models import Ticket
+from .models import Ticket, User
 
 views = Blueprint("views", __name__)
 
@@ -32,17 +32,25 @@ def tickets():
     formatted_headings = []
     for header in headings:
         if 'user_id' in str(header):
-            continue
-        formatted_headings.append(
-            str(header).replace('ticket.', '').capitalize().replace('_', ' '))
+            formatted_headings.append('Owner')
+        else:
+            formatted_headings.append(
+                str(header).replace('ticket.', '').capitalize().replace('_', ' '))
 
+    tickets = db.session.query(Ticket).filter(
+        Ticket.user_id == current_user.id)
     tickets = db.session.query(Ticket)
-    filtered_tickets = []
+    my_tickets = []
+    remaining_tickets = []
     for ticket in tickets:
-        filtered_tickets.append(
-            [ticket.id, ticket.title, ticket.description, ticket.date_added])
+        if ticket.user_id == current_user.id:
+            my_tickets.append(
+                [ticket.id, ticket.user_id, ticket.title, ticket.description, ticket.date_added])
+        else:
+            remaining_tickets.append(
+                [ticket.id, ticket.user_id, ticket.title, ticket.description, ticket.date_added])
 
-    return render_template("tickets.html", user=current_user, headings=formatted_headings, data=filtered_tickets)
+    return render_template("tickets.html", user=current_user, headings=formatted_headings, my_tickets=my_tickets, remaining_tickets=remaining_tickets)
 
 
 @views.route("/create/", methods=['GET', 'POST'])
@@ -51,13 +59,30 @@ def create():
         title = request.form.get('title')
         description = request.form.get('description')
 
-        new_ticket = Ticket(title=title, description=description)
+        new_ticket = Ticket(
+            title=title, description=description, user=current_user)
         db.session.add(new_ticket)
         db.session.commit()
         flash('Ticket created!', category='success')
         return redirect(url_for('views.tickets'))
 
     return render_template("create_ticket.html", user=current_user)
+
+
+@views.route("/update/<int:id>", methods=['GET', 'POST'])
+def update(id):
+    ticket_to_update = Ticket.query.get_or_404(id)
+    if request.method == 'POST':
+        ticket_to_update.title = request.form.get('title')
+        ticket_to_update.description = request.form.get('description')
+
+        try:
+            db.session.commit()
+            flash('Ticket Updated!', category='success')
+            return redirect(url_for('views.tickets'))
+        except:
+            raise RuntimeError("There was an error when updating the ticket")
+    return render_template("update_ticket.html", user=current_user, ticket_to_update=ticket_to_update)
 
 
 @views.route("/delete/<int:id>")
@@ -70,6 +95,26 @@ def delete(id):
         return redirect(url_for('views.tickets'))
     except:
         return RuntimeError("Error when deleting the ticket")
+
+
+@views.route("/users/")
+def users():
+    # Get the headings for the Ticket table
+    headings = User.__table__.columns
+    formatted_headings = []
+    for header in headings:
+        if 'password' in str(header):
+            continue
+        formatted_headings.append(
+            str(header).replace('user.', '').capitalize().replace('_', ' '))
+
+    users = db.session.query(User)
+    filtered_users = []
+    for user in users:
+        filtered_users.append(
+            [user.id, user.email, user.first_name, user.last_name, user.role])
+
+    return render_template("users.html", user=current_user, headings=formatted_headings, data=filtered_users)
 
 
 @views.route("/api/data")
